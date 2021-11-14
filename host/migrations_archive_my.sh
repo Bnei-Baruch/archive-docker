d3926a9084f388de1fc13a0f4ab474ec2d7d011f
Edo Shor <edo@cloudinary.com>
2021-11-14 05:09:33 +0200

    take migrations from next to be release docker image

--- host/migrations_archive_my.sh
+++ host/migrations_archive_my.sh
@@ -6,10 +6,11 @@ set -e
 tmp_dir=$(mktemp -d -t migrations-"$(date +%Y-%m-%d-%H-%M-%S)"-XXXXXXXXXX)
 echo "$tmp_dir"
 
-docker cp archive-docker_archive_my_1:/app/migrations "$tmp_dir"
-
 eval "$(shdotenv -d docker)"
-
+dummy="dummy_archive_my"
+docker create --name ${dummy} bneibaruch/archive_my:${ARCHIVE_MY_VERSION}
+docker cp ${dummy}:/app/migrations "$tmp_dir"
+ls -laR $tmp_dir
 docker run --rm -v "$tmp_dir/migrations/mydb":/migrations --network archive-docker_backend migrate/migrate -path=/migrations/ -database postgres://$MY_USER:$MY_PASSWORD@postgres_my/mydb?sslmode=disable up
-
+docker rm -f ${dummy}
 rm -rf "$tmp_dir"
