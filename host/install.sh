82bd9c073f06e8f828f0f3c581cf5ae29cbd5048
Edo Shor <edo@cloudinary.com>
2022-04-17 05:30:56 +0300

    Backup via http (#3)

--- host/install.sh
+++ host/install.sh
@@ -10,7 +10,8 @@ yum install -y \
   device-mapper-persistent-data \
   lvm2 \
   git \
-  jq
+  jq \
+  httpd-tools
 
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 
